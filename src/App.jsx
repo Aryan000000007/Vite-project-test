7e704f75860f7a8e3a1787fb261a27dade7c3e9e
Aryan000000007 <aryan.s@techouts.com>
2024-09-20 08:29:17 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
 const App = () => {
+const name ='Aryan Singh'
+const a= 12
+const b =45
+const login = true
+const name1 =['sam', 'karan','pintu','mohit','sara']
+
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+
+    <div className='text-5xl'>App {name}</div><br />
+
+    <p style={{color:'red',fontSize:'24px'}}>The Sum of {a} and {b} is {a+b} </p>
+
+    <ul>
+      {name1.map((name , index)=> (
+        <li key={index}>{name}</li>
+      ))}
+    </ul>
+
+        {login ? <h1>Hello {name}</h1>:<h1>Hello guest</h1>}
+
+    </>
   )
 }
 
